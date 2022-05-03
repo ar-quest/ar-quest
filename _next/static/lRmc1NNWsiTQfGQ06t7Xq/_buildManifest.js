e1ee8fea81e851120b0cd56c34a38e3a925c9d31
Firesieht <firesieht@mail.ru>
2022-05-03 21:42:22 +0300

    init

---
+++ _next/static/lRmc1NNWsiTQfGQ06t7Xq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/398-bfcf197e014fb386.js","static/css/73bea4e82777b85e.css","static/chunks/pages/index-4e311fa89a6bf5d8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
